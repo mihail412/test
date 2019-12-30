466fb943856cb0fe48895ab33bf82bae5ad1afe6
nazar-serg <nazarenkoenergodar@gmail.com>
2019-12-30 16:27:22 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/*var search = document.getElementById('inline-search').value;
+var textOriginal = document.getElementById('article').innerHTML;
+*/
+
+let a = 'Hello World!';
+function first() {
+  let b = 25;  
+  console.log('Inside first function');
+}
+first();
+console.log('Inside global execution context');
